da9a1cc98b96a7a662c0b93ab7ff9c345946e6f6
Benjamin Wishart <ben@envisageny.com>
2015-04-04 21:24:36 -0400

    Initial Commit

---
+++ BoardGameLibrary/BoardGameLibrary.h
@@ -0,0 +1,13 @@
+//
+//  BoardGameLibrary.h
+//  BoardGameLibrary
+//
+//  Created by Benjamin Wishart on 2015-04-04.
+//  Copyright (c) 2015 Benjamin Wishart. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BoardGameLibrary : NSObject
+
+@end
---
+++ BoardGameLibrary/BoardGameLibrary.m
@@ -0,0 +1,13 @@
+//
+//  BoardGameLibrary.m
+//  BoardGameLibrary
+//
+//  Created by Benjamin Wishart on 2015-04-04.
+//  Copyright (c) 2015 Benjamin Wishart. All rights reserved.
+//
+
+#import "BoardGameLibrary.h"
+
+@implementation BoardGameLibrary
+
+@end
